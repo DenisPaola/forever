20cbb859f8214add60ec4a2b866d48ec9b121298
denispaola <denislozanoc@gmail.com>
2023-05-12 07:39:51 +0200

    reto 2 - dia 5

---
+++ reto2/index.js
@@ -0,0 +1,68 @@
+function suma(operacion, op1, op2 = 1) {
+    if (operacion == "sum") {
+        let resultado = op1 + op2;
+        console.log("suma es igual a" + resultado)
+        return resultado;
+    }
+};
+
+
+function resta(operacion, op1, op2 = 1) {
+    if (operacion == "subs") {
+        let resultado = op1 - op2;
+        console.log("resta es igual a" + resultado)
+        return resultado;
+    }
+};
+
+
+function multiplicacion(operacion, op1, op2 = 1) {
+    if (operacion == "mult") {
+        let resultado = op1 * op2;
+        console.log("multiplicacion es igual a" + resultado)
+        return resultado;
+    }
+};
+
+
+function division(operacion, op1, op2 = 1) {
+    if (operacion == "div") {
+        let resultado = op1 / op2;
+        console.log("division es igual a" + resultado)
+        return resultado;
+    }
+};
+
+function cuadrado(numero) {
+    const result = numero * numero;
+    console.log('El cuadrado de ' + numero + ' es ' + result);
+    return result;
+}
+
+
+
+//Main===================================================================
+
+suma("sum", 12,);
+resta("subs", 20,);
+multiplicacion("mult", 20,);
+division("div", 36,);
+cuadrado(8);
+
+suma("sum", 12,);
+resta("subs", -12,);
+multiplicacion("mult", 0,);
+division("div", 0,);
+cuadrado(2);
+
+suma("sum", -1,);
+resta("subs", 1,);
+multiplicacion("mult", 1,);
+division("div", 1,);
+cuadrado(10);
+
+
+
+
+module.exports = { suma, resta, multiplicacion, division }
+
---
+++ reto2/run.js
@@ -0,0 +1,5 @@
+let mylib = require("./index");
+mylib.suma(12,1);
+mylib.resta(20,1);
+mylib.multiplicacion(20,1);
+mylib.division(36,1);
